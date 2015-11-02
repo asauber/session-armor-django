41c4a9b5bdc6956b4948952687c52fc9201a0cc4
Andrew Sauber <a3sauber@gmail.com>
2015-11-01 20:41:03 -0500

    parse client headers, identify ready state

--- sessionarmor_django/middleware.py
+++ sessionarmor_django/middleware.py
@@ -10,6 +10,38 @@ This software is licensed under the MIT open source license. See LICENSE.txt
 from django.conf import settings
 
 
+CLIENT_READY = 'ready'
+
+
+def header_to_dict(header, outer_sep=';', inner_sep=':'):
+    '''
+    Takes a header value of the form:
+    c:<base64data0>;T_re:1367448031;h:<base64data1>;ignored0;
+    Returns a dictionary:
+    {
+        'c': <base64data0>,
+        'T_re': 1367448031,
+        'h': <base64data1>,
+    }
+    '''
+    kvs = header.split(outer_sep)
+    # remove empty tokens
+    kvs = (kv for kv in kvs if kv != '')
+    # split key/value tokens
+    kvs = (kv.split(inner_sep) for kv in kvs)
+    # parse key/value tokens
+    d = {kv[0]: kv[1] for kv in kvs if len(kv) == 2}
+    return d
+
+
+def get_client_state(header):
+    '''
+    Given a header dictionary, return the name of the client state
+    '''
+    if len(header) == 1 and header.keys()[0] == 'r':
+        return CLIENT_READY
+
+
 class SessionArmorMiddleware(object):
     '''
     Implementation of the Session Armor protocol.
@@ -20,14 +52,10 @@ class SessionArmorMiddleware(object):
     def process_request(self, request):
         '''
         Process stages of the Session Armor protocol for incoming requests
-
-        For now, print all headers from each request to stdout
         '''
-        if not settings.PRINTHEADERS:
-            return
-
-        for name, value in request.META.iteritems():
-            print name, ':', value
+        header = header_to_dict(request.META['HTTP_X_S_ARMOR'])
+        state = get_client_state(header)
+        print state
 
     def process_response(self, request, response):
         '''
